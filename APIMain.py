b6caac09d7f29ca047f7480f656c6a7cda266718
krisHans3n <krishans3n@gmail.com>
2021-03-28 21:51:27 +0100

    update

--- APIMain.py
+++ APIMain.py
@@ -3,6 +3,6 @@ from conduit.settings import ProdConfig, DevConfig, runtime_settings
 
 THREADING, PORT, DEBUG = runtime_settings()
 
-if __name__ == '__main__':
-    app = create_app(DevConfig)
-    app.run(threaded=True, port=5000, debug=True)
+# if __name__ == '__main__':
+app = create_app(ProdConfig)
+app.run(threaded=True, port=5000, debug=True)
