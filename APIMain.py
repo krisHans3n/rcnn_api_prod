73c83585b54f0fd55b2c7f5f373b1ab1717ebcb2
krisHans3n <krishans3n@gmail.com>
2021-03-28 23:46:26 +0100

    update readme

--- APIMain.py
+++ APIMain.py
@@ -2,7 +2,8 @@ from conduit.app import create_app
 from conduit.settings import ProdConfig, DevConfig, runtime_settings
 
 THREADING, PORT, DEBUG = runtime_settings()
+app = create_app(DevConfig)
 
 if __name__ == '__main__':
-    app = create_app(DevConfig)
+
     app.run(host='0.0.0.0', port=5000)  # (threaded=True, port=5000, debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+Flask API entry point to microservice image fraud detection/ classifier. consumes URLs and outputs JSON analytics
